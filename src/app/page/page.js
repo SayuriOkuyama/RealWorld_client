737a0c26db9473707ef94f15eeed0fa2f242ea8a
SayuriOkuyama <mosyaneko77@gmail.com>
2024-01-28 09:58:20 +0900

    編集機能を修正

--- src/app/page/page.js
+++ src/app/page/page.js
@@ -97,16 +97,6 @@ export default function Page() {
               </div>
 
               <ul className="pagination">
-                {/* <li className="page-item active">
-                  <a className="page-link" href="">
-                    1
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="">
-                    2
-                  </a>
-                </li> */}
                 <Pagination pageData={pageData} />
               </ul>
             </div>
