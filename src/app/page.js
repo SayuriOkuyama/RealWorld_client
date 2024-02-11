f8af8ccbb88bcaa050cbf50ebb52b04b10518c7e
SayuriOkuyama <mosyaneko77@gmail.com>
2024-02-12 01:16:04 +0900

    docker コンテナ化に伴い、ファイル追加・削除。Build エラー解消。

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 import Pagination from '@/components/Pagination'
 import AllArticles from '@/components/AllArticles'
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
 const Home = () => {
   const [pageData, setPageData] = useState()
@@ -19,7 +20,7 @@ const Home = () => {
         setPageData(response.data)
         setArticles(response.data.data)
       } catch (err) {
-        console.log(err)
+        // console.log(err)
       }
     }
     getArticles()
@@ -27,73 +28,75 @@ const Home = () => {
 
   return (
     <>
-      <div className="home-page">
-        <div className="banner">
-          <div className="container">
-            <h1 className="logo-font">conduit</h1>
-            <p>A place to share your knowledge.</p>
+      <Suspense>
+        <div className="home-page">
+          <div className="banner">
+            <div className="container">
+              <h1 className="logo-font">conduit</h1>
+              <p>A place to share your knowledge.</p>
+            </div>
           </div>
-        </div>
-        <div className="container page">
-          <div className="row">
-            <div className="col-md-9">
-              <div className="feed-toggle">
-                <ul className="nav nav-pills outline-active">
-                  <li className="nav-item">
-                    <a className="nav-link" href="">
-                      あなたの投稿
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link active" href="">
-                      すべての投稿
-                    </a>
-                  </li>
+          <div className="container page">
+            <div className="row">
+              <div className="col-md-9">
+                <div className="feed-toggle">
+                  <ul className="nav nav-pills outline-active">
+                    <li className="nav-item">
+                      <a className="nav-link" href="">
+                        あなたの投稿
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link active" href="">
+                        すべての投稿
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <AllArticles articles={articles} />
+
+                <ul className="pagination">
+                  <Pagination pageData={pageData} />
                 </ul>
               </div>
 
-              <AllArticles articles={articles} />
+              <div className="col-md-3">
+                <div className="sidebar">
+                  <p>Popular Tags</p>
 
-              <ul className="pagination">
-                <Pagination pageData={pageData} />
-              </ul>
-            </div>
-
-            <div className="col-md-3">
-              <div className="sidebar">
-                <p>Popular Tags</p>
-
-                <div className="tag-list">
-                  <a href="" className="tag-pill tag-default">
-                    programming
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    javascript
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    emberjs
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    angularjs
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    react
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    mean
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    node
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    rails
-                  </a>
+                  <div className="tag-list">
+                    <a href="" className="tag-pill tag-default">
+                      programming
+                    </a>
+                    <a href="" className="tag-pill tag-default">
+                      javascript
+                    </a>
+                    <a href="" className="tag-pill tag-default">
+                      emberjs
+                    </a>
+                    <a href="" className="tag-pill tag-default">
+                      angularjs
+                    </a>
+                    <a href="" className="tag-pill tag-default">
+                      react
+                    </a>
+                    <a href="" className="tag-pill tag-default">
+                      mean
+                    </a>
+                    <a href="" className="tag-pill tag-default">
+                      node
+                    </a>
+                    <a href="" className="tag-pill tag-default">
+                      rails
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </Suspense>
     </>
   )
 }
--- src/app/page/page.js
+++ src/app/page/page.js
@@ -4,6 +4,7 @@ import axios from '@/lib/axios'
 import React, { useEffect, useState } from 'react'
 import Pagination from '@/components/Pagination'
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
 export default function Page() {
   const [pageData, setPageData] = useState()
@@ -15,12 +16,12 @@ export default function Page() {
     const getArticles = async () => {
       try {
         const response = await axios.get(`api/articles?page=${page}`)
-        console.log(response.data)
+        // console.log(response.data)
 
         setPageData(response.data)
         setArticles(response.data.data)
       } catch (err) {
-        console.log(err)
+        // console.log(err)
       }
     }
     getArticles()
@@ -28,114 +29,116 @@ export default function Page() {
 
   return (
     <>
-      <div className="home-page">
-        <div className="banner">
-          <div className="container">
-            <h1 className="logo-font">conduit</h1>
-            <p>A place to share your knowledge.</p>
+      <Suspense>
+        <div className="home-page">
+          <div className="banner">
+            <div className="container">
+              <h1 className="logo-font">conduit</h1>
+              <p>A place to share your knowledge.</p>
+            </div>
           </div>
-        </div>
-        <div className="container page">
-          <div className="row">
-            <div className="col-md-9">
-              <div className="feed-toggle">
-                <ul className="nav nav-pills outline-active">
-                  <li className="nav-item">
-                    <a className="nav-link" href="">
-                      Your Feed
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link active" href="">
-                      Global Feed
-                    </a>
-                  </li>
+          <div className="container page">
+            <div className="row">
+              <div className="col-md-9">
+                <div className="feed-toggle">
+                  <ul className="nav nav-pills outline-active">
+                    <li className="nav-item">
+                      <a className="nav-link" href="">
+                        Your Feed
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link active" href="">
+                        Global Feed
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div>
+                  {articles.map(article => {
+                    return (
+                      <>
+                        <div className="article-preview">
+                          <div className="article-meta">
+                            <a href="/profile/eric-simons">
+                              <img src="http://i.imgur.com/Qr71crq.jpg" />
+                            </a>
+                            <div className="info">
+                              <a href="/profile/eric-simons" className="author">
+                                Eric Simons
+                              </a>
+                              <span className="date">January 20th</span>
+                            </div>
+                            <button className="btn btn-outline-primary btn-sm pull-xs-right">
+                              <i className="ion-heart" /> 29
+                            </button>
+                          </div>
+                          <Link
+                            href={`/article/${article.id}`}
+                            className="preview-link">
+                            <h1>{article.title}</h1>
+                            <p>{article.about}</p>
+                            <span>Read more...</span>
+                            <ul className="tag-list">
+                              {article.tags.map(tag => {
+                                return (
+                                  <li
+                                    key={`${article.id}.${tag.id}`}
+                                    className="tag-default tag-pill tag-outline">
+                                    {tag.name}
+                                  </li>
+                                )
+                              })}
+                            </ul>
+                          </Link>
+                        </div>
+                      </>
+                    )
+                  })}
+                </div>
+
+                <ul className="pagination">
+                  <Pagination pageData={pageData} />
                 </ul>
               </div>
 
-              <div>
-                {articles.map(article => {
-                  return (
-                    <>
-                      <div className="article-preview">
-                        <div className="article-meta">
-                          <a href="/profile/eric-simons">
-                            <img src="http://i.imgur.com/Qr71crq.jpg" />
-                          </a>
-                          <div className="info">
-                            <a href="/profile/eric-simons" className="author">
-                              Eric Simons
-                            </a>
-                            <span className="date">January 20th</span>
-                          </div>
-                          <button className="btn btn-outline-primary btn-sm pull-xs-right">
-                            <i className="ion-heart" /> 29
-                          </button>
-                        </div>
-                        <Link
-                          href={`/article/${article.id}`}
-                          className="preview-link">
-                          <h1>{article.title}</h1>
-                          <p>{article.about}</p>
-                          <span>Read more...</span>
-                          <ul className="tag-list">
-                            {article.tags.map(tag => {
-                              return (
-                                <li
-                                  key={`${article.id}.${tag.id}`}
-                                  className="tag-default tag-pill tag-outline">
-                                  {tag.name}
-                                </li>
-                              )
-                            })}
-                          </ul>
-                        </Link>
-                      </div>
-                    </>
-                  )
-                })}
-              </div>
+              <div className="col-md-3">
+                <div className="sidebar">
+                  <p>Popular Tags</p>
 
-              <ul className="pagination">
-                <Pagination pageData={pageData} />
-              </ul>
-            </div>
-
-            <div className="col-md-3">
-              <div className="sidebar">
-                <p>Popular Tags</p>
-
-                <div className="tag-list">
-                  <a href="" className="tag-pill tag-default">
-                    programming
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    javascript
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    emberjs
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    angularjs
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    react
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    mean
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    node
-                  </a>
-                  <a href="" className="tag-pill tag-default">
-                    rails
-                  </a>
+                  <div className="tag-list">
+                    <a href="" className="tag-pill tag-default">
+                      programming
+                    </a>
+                    <a href="" className="tag-pill tag-default">
+                      javascript
+                    </a>
+                    <a href="" className="tag-pill tag-default">
+                      emberjs
+                    </a>
+                    <a href="" className="tag-pill tag-default">
+                      angularjs
+                    </a>
+                    <a href="" className="tag-pill tag-default">
+                      react
+                    </a>
+                    <a href="" className="tag-pill tag-default">
+                      mean
+                    </a>
+                    <a href="" className="tag-pill tag-default">
+                      node
+                    </a>
+                    <a href="" className="tag-pill tag-default">
+                      rails
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </Suspense>
     </>
   )
 }
